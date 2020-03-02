98f9675dc7a58cd5011af1abff0583b76365930c
Chinh Hoang <quangchinh11t1@gmail.com>
2020-03-02 11:46:12 +0000

    Remove dead code.

--- spec/shared/contexts/movie_context.rb
+++ spec/shared/contexts/movie_context.rb
@@ -156,14 +156,6 @@ RSpec.shared_context 'movie class' do
 
     class Supplier
       attr_accessor :id, :account_id
-
-      def account
-        if account_id
-          a = Account.new
-          a.id = account_id
-          a
-        end
-      end
     end
 
     class Account
@@ -293,18 +285,12 @@ RSpec.shared_context 'movie class' do
       include FastJsonapi::ObjectSerializer
       set_type :movie
       attributes :name, :release_year
-      attribute :title_with_year do |record|
-        "#{record.name} (#{record.release_year})"
-      end
     end
 
     class MovieSerializerWithAttributeBlock
       include FastJsonapi::ObjectSerializer
       set_type :movie
       attributes :name, :release_year
-      attribute :title_with_year do |record|
-        "#{record.name} (#{record.release_year})"
-      end
     end
 
     class AgencySerializer
